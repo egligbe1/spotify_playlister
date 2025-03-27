9621ba469a732d7d913b661021d5b17e4c54f4a2
egligbe1 <skeledon246@gmail.com>
2025-03-27 21:12:10 +0000

    modified:   run_playlist_updater.bat

--- run_playlist_updater.bat
+++ run_playlist_updater.bat
@@ -1,5 +1,5 @@
 @echo off
-cd /d "D:\Academics\Desktop\PROJECTS\WebDev\spotify_playlister"
+cd /d "D:\Academics\Desktop\PROJECTS\WebDev\SpotifyPlaylistUpdater\spotify_playlister\run_playlist_updater.bat"
 call .\venv\Scripts\activate
 python main.py
 deactivate
