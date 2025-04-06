833e75b749f9e5c799607ceaedf19f85465f57c0
Emmanuel Gligbe <60310979+egligbe1@users.noreply.github.com>
2025-04-06 07:56:30 +0000

    Update main.py

--- main.py
+++ main.py
@@ -118,7 +118,7 @@ def update_playlist_metadata(sp, target_playlist):
 # Main playlist update logic
 def update_playlist():
     now = datetime.datetime.now()
-    if now.weekday() != 5 or now.hour != 0 or now.minute > 5:
+    if now.weekday() != 6 or now.hour != 8 or now.minute > 5:
         logging.info(f"Not scheduled time. Current time: {now} UTC. Expected: Saturday 00:00-00:05 UTC")
         return
 
